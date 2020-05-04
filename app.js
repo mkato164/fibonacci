8f6ecc910edb918d52b37afdb52cb3952fa50a50
mkato164 <tkm15gd@gmail.com>
2020-05-04 16:00:42 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 'use strict';
+function fib(n){
+    if(n === 0){
+        return 0;
+    }else if(n === 1){
+        return 1;
+    }
+
+    return fib(n-2) + fib (n-1);
+}
+const length = 40;
+for (let index = 0; index <= length; index++) {
+    console.log(index + "番目の数値は" + fib(index));    
+}
+
